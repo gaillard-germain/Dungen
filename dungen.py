06eab02740da188c90cec09be9c0c117acab47d7
Gaillard Germain <gaillard.germain@gmail.com>
2022-08-30 23:13:58 +0200

    Add plan() and brick_up() methods.

--- dungen.py
+++ dungen.py
@@ -11,11 +11,10 @@ class Dungen:
         self.iter = iter
         self.branches = []
         self.floor = ' '
-        self.void = 'X'
+        self.wall = '#'
+        self.void = '.'
 
     def get_neighbors(self, point, diag=False, gap=2):
-        """ Return a list of coords adjacent to a point (x, y) using range """
-
         x, y = point
         delta = gap
 
@@ -30,26 +29,36 @@ class Dungen:
             for i in (-delta, delta):
                 coord_x = (x+i, y)
                 coord_y = (x, y+i)
+
                 if coord_x not in self.map:
                     yield (x+i, y)
+
                 if coord_y not in self.map:
                     yield (x, y+i)
+
+    def check(self, coord):
+        for tile in self.get_neighbors(coord):
+            if len(list(self.get_neighbors(tile, True, 1))) == 8:
+                yield tile
 
     def update_minmax(self, coord):
         if coord[0] < self.min_x:
             self.min_x = coord[0]
         elif coord[0] > self.max_x:
             self.max_x = coord[0]
+
         if coord[1] < self.min_y:
             self.min_y = coord[1]
         elif coord[1] > self.max_y:
             self.max_y = coord[1]
 
-    def create_corridor(self, coord, room=False):
-        neighbors = list(self.get_neighbors(coord))
+    def build(self, coord, room=False):
+        neighbors = list(self.check(coord))
+
         if neighbors:
             next = choice(neighbors)
             self.map[next] = self.floor
+
             if room:
                 room = self.get_neighbors(next, True, 1)
                 for tile in room:
@@ -63,45 +72,50 @@ class Dungen:
 
             return next
 
-    def create_room(self):
-        pass
-
-    def dig(self, iter, current):
-        current = current
+    def plan(self, iter, current):
         for i in range(iter):
             room = False
-            if randint(1, 100) < 5:
+
+            if randint(1, 100) < 10 or i == iter-1:
                 room = True
-            if randint(1, 100) < 10:
+
+            if randint(1, 100) < 20:
                 self.branches.append(current)
-                self.dig(randint(5, 10), current)
-            if i == iter-1:
-                current = self.create_corridor(current, True)
-            else:
-                current = self.create_corridor(current, room)
+                self.plan(randint(3, 15), current)
+
+            current = self.build(current, room)
+
             if not current:
                 if self.branches:
                     current = self.branches.pop()
+
                 else:
                     break
+
+    def brick_up(self):
+        for tile in self.map.copy().keys():
+            for coord in self.get_neighbors(tile, True, 1):
+                self.map[coord] = self.wall
 
     def gen(self):
         current = (0, 0)
         self.map[current] = self.floor
-        self.dig(self.iter, current)
+        self.plan(self.iter, current)
+        self.brick_up()
 
     def display(self):
         for y in range(self.min_y-1, self.max_y+2, 1):
             line = ''
             for x in range(self.min_x-1, self.max_x+2, 1):
                 if (x, y) in self.map:
-                    line += '{}'.format(self.map[(x, y)])
+                    line += self.map[(x, y)]
                 else:
-                    line += '{}'.format(self.void)
+                    line += self.void
+
             print(line)
 
 
 if __name__ == "__main__":
-    dungen = Dungen(50)
+    dungen = Dungen(30)
     dungen.gen()
     dungen.display()
